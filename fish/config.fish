3e9c3cb81c24ed4649a7997cb7122f3eff710d8f
Kenneth Vestergaard <kvs@binarysolutions.dk>
2013-02-27 12:44:18 +0100

    Set PATH before including other config.

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,12 @@
+set -x PATH /usr/local/bin /usr/local/sbin $HOME/bin $PATH
+
 . ~/.config/fish/aliases.fish
 . ~/.config/fish/prompt.fish
 . ~/.config/fish/complete-knife.fish
 . ~/.config/fish/solarize.fish
+. ~/.config/fish/rubies.fish
 
 set -e fish_greeting
-
-# Path
-set -x PATH /usr/local/bin /usr/local/sbin $HOME/bin $PATH /usr/local/lib/ruby/gems/1.9.1/bin
 
 # External tool options
 set -x LC_ALL en_US.UTF-8
